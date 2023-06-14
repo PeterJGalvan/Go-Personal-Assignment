11961438081b0078507ea4f61dc328a12a5341ad
Peter Galvan <Petergalvan99@outlook.com>
2023-06-13 21:31:01 -0500

    Finished with project

--- algo/algo.go
+++ algo/algo.go
@@ -1,19 +1,58 @@
 package algo
 
-func readInputs(fileName string) ([]float32, error) {
-	/*
-		Write code to read in an input file and format
-		the values into []float32
-	*/
+import (
+	"encoding/json"
+	"math"
+	"os"
+)
 
-	return nil, nil
+func readInputs(fileName string) ([][]float32, error) {  
+  fileData, error := os.ReadFile(fileName)
+  
+  if error != nil {
+    return nil, error
+  }
+
+  var data struct {
+    Input [][]float32
+  }
+
+  // Unmarshal returns a error you need to pass a pointer in 2nd parameter 
+  // for where the values are returned to
+  error = json.Unmarshal(fileData, &data)
+
+  if error != nil {
+    return nil, error
+  }
+
+  return data.Input, nil
 }
 
 func countAverages(fileName string) ([]float32, error) {
-	/*
-		// call readInputs() to get values
-		Fill out solution here.
-	*/
+  jsonInput, error := readInputs(fileName)
 
-	return nil, nil
+  if error != nil {
+    return nil, error
+  }
+
+  returnValue := []float32{}
+
+  for _, currentArray := range jsonInput {
+    var sum float32 = 0
+
+    for _, currentItem := range currentArray {
+      sum += currentItem
+    }
+    
+    averageOfCurrentArray := sum / 2
+    
+    // checking if averageOfCurrentArray is a number math.IsNaN only takes float64
+    if(math.IsNaN(float64(averageOfCurrentArray))) {
+      returnValue = append(returnValue, 0)
+    } else {
+      returnValue = append(returnValue, averageOfCurrentArray)
+    }
+  }
+
+  return returnValue, nil
 }
--- algo/algo_test.go
+++ algo/algo_test.go
@@ -1,15 +1,96 @@
 package algo
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_countAverages(t *testing.T) {
-	/*
-		Test solution here using a test table.
-	*/
+  testCases := []struct {
+    name string
+    filePath string
+    expected []float32
+    isError  bool
+  }{
+    {
+      name: "Good Case",
+      filePath: "./inputs/good.json",
+      expected: []float32{138.5, 0, 66, 237},
+      isError: false,
+    },
+    {
+      name: "Bad Case",
+      filePath: "./inputs/bad.json",
+      isError:  true,
+    },
+  }
+
+  for _, currentTestCase := range testCases {
+    t.Run(currentTestCase.name, func(t *testing.T) {
+      inputs, error := countAverages(currentTestCase.filePath)
+
+      // checking if there is a error and there should be no error
+      if(error != nil && !currentTestCase.isError) {
+        t.Errorf("Expected No Errors, got: %v", error)
+      }
+
+      // checking if there is no errors and there should be a error
+      if(error == nil && currentTestCase.isError) {
+        t.Errorf("Expected No Errors, got: %v", error)
+      }
+
+      // checking for expected output to equal returned inputs
+      if !reflect.DeepEqual(inputs, currentTestCase.expected) {
+        t.Errorf("Expected inputs: %v, got: %v", currentTestCase.expected, inputs)
+      }
+    })
+  }
 }
 
+
 func Test_readInputs(t *testing.T) {
-	/*
-		Test solution here using a test table.
-	*/
+  testCases := []struct {
+    name     string
+    filePath string
+    expected [][]float32
+    isError  bool
+  }{
+    {
+      name: "Good Case",
+      filePath: "./inputs/good.json",
+      expected: [][]float32{
+        {87, 23, 57, 87, 23},
+        {},
+        {75, 34, 23},
+        {23, 45, 83, 239, 28, 56},
+      },
+      isError: false,
+    },
+    {
+      name: "Bad Case",
+      filePath: "./inputs/bad.json",
+      isError: true,
+    },
+  }
+
+  for _, currentTestCase := range testCases {
+    t.Run(currentTestCase.name, func(t *testing.T) {
+      inputs, error := readInputs(currentTestCase.filePath)
+
+      // checking if there is a error and there should be no error
+      if(error != nil && !currentTestCase.isError) {
+        t.Errorf("Expected No Errors, got: %v", error)
+      }
+
+      // checking if there is no errors and there should be a error
+      if(error == nil && currentTestCase.isError) {
+        t.Errorf("Expected No Errors, got: %v", error)
+      }
+
+      // checking for expected output to equal returned inputs
+      if !reflect.DeepEqual(inputs, currentTestCase.expected) {
+        t.Errorf("Expected inputs: %v, got: %v", currentTestCase.expected, inputs)
+      }
+    })
+	}
 }
